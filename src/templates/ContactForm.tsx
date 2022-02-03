5ad145bcf43bcd604a34e6204c72aafed63f9257
M.Bellucci <delbetu@gmail.com>
2022-02-03 19:44:11 -0300

    style form

--- src/templates/ContactForm.tsx
+++ src/templates/ContactForm.tsx
@@ -1,35 +1,46 @@
+import { Section } from '../layout/Section';
+
 const ContactForm = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true" action="/">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <Section>
+      <div className="flex justify-center">
+        <div className="block p-6 rounded-lg shadow-lg bg-white w-1/2">
+          <form name="contact" method="POST" data-netlify="true" action="/">
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="form-group mb-6">
+              <input
+                type="text"
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                id="name1"
+                placeholder="Name"
+              />
+            </div>
+            <div className="form-group mb-6">
+              <input
+                type="email"
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                id="email1"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="form-group mb-6">
+              <textarea
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none "
+                id="textarea1"
+                rows={10}
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </Section>
   );
 };
 
