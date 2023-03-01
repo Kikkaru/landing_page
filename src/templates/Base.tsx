9ba6a7bda34187369a57cdf3c70447626bad042f
M. Bellucci <delbetu@gmail.com>
2023-03-01 09:21:47 -0300

    Under Construction Version

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,19 +1,26 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { Banner } from './Banner';
-import ContactForm from './ContactForm';
-import { Footer } from './Footer';
+// import { Banner } from './Banner';
+// import ContactForm from './ContactForm';
+// import { Footer } from './Footer';
 import { Hero } from './Hero';
-import { VerticalFeatures } from './VerticalFeatures';
+// import { VerticalFeatures } from './VerticalFeatures';
+
+// const OldBase = () => (
+//   <div className="antialiased text-gray-600">
+//     <Meta title={AppConfig.title} description={AppConfig.description} />
+//     <Hero />
+//     <VerticalFeatures />
+//     <Banner />
+//     <ContactForm />
+//     <Footer />
+//   </div>
+// );
 
 const Base = () => (
   <div className="antialiased text-gray-600">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
-    <VerticalFeatures />
-    <Banner />
-    <ContactForm />
-    <Footer />
   </div>
 );
 
